7f55ce47cd406d0120b9774b210699dfff749cdc
Kubilay <kubilayckmk@gmail.com>
2023-07-06 15:27:23 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./global.scss";
+
+function App() {
+    return (
+        <div>
+            <h1>Hello Maiko 1!</h1>
+        </div>
+    );
+}
+
+export default App;
